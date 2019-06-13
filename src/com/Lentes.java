e37415d3166524b969a893fbeaf97d44f9d03858
ITSLOJA <ITSLOJA@ISTLOJA08>
2019-06-13 11:00:22 -0500

    Sincronicaación ISTLOJA

---
+++ src/com/Lentes.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author ITSLOJA
+ */
+////////public class Lentes {
+////////    private float alcance;
+////////    private String color;
+////////    private String tamaño;
+////////    private String modelo;
+////////    Declare metodos
+////////    public void alcancedevista(){
+////////        System.out.println("Los lentes tienen un alcance de 4.50 ");
+////////    }
+////////    public void colordelentes(){
+////////        System.out.println("Los lentes son de color ");
+////////    }
+////////    public void tamañodelentes(String info){
+////////        System.out.println(info);
+////////    }
+////////    public static void main(String[] args) {
+////////        Lentes primerobjeto=new Lentes();//Eh creado mi primer objeto
+////////        primerobjeto.alcancedevista();
+////////        primerobjeto.tamañodelentes("Mediano");
+////////    }
+////////}
